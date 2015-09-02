0142b4e858e0cc6786fdef8d300600e0608ffbe8
nguyenuit <nguyenuit@gmail.com>
2015-09-02 10:11:10 +0700

    v1.59 ads

--- app/views/pages/videos.blade.php
+++ app/views/pages/videos.blade.php
@@ -20,6 +20,7 @@
             
         </ul>
     </div>
+ 
     <div id="paging-buttons" class="paging-buttons">
         <a href="#" onclick="return false;" class="previous disabled" <?php if ($page ==1) echo 'style="opacity:0.1"' ?>>« Trang trước</a>
         <a href="<?php echo $base_url . '/videos/'.($page + 1)?>" class="older">Trang sau »</a>
@@ -30,8 +31,8 @@
 @section('sidebar_content')
 
 @include('includes.topusers')
+@include('includes.ads')
 @include('includes.photo_sidebar')
-
 @stop
 
 
